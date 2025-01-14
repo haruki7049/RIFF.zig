77bba3b5f71ca7b94febb0b36c779b997556cd0b
haruki7049 <tontonkirikiri@gmail.com>
2025-01-14 21:52:30 +0900

    WIP

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,35 @@
-const std = @import("std");
-const testing = std.testing;
+pub const Chunk = struct {
+    four_cc: *const [4]u8,
+    size: u32,
+    payload: ?[]u8,
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+    const InitOptions = struct {
+        four_cc: [4]u8,
+        size: u32,
+        payload: ?[]u8,
+    };
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+    pub fn init(options: InitOptions) Chunk {
+        return Chunk{
+            .four_cc = options.four_cc,
+            .size = options.size,
+            .payload = options.payload,
+        };
+    }
+
+    pub fn size(self: Chunk) !u64 {
+        if (self.payload == null) {
+            return 4;
+        } else {
+            return 0;
+        }
+    }
+};
+
+test "chunk-core-checks" {
+    _ = Chunk.init(.{
+        .four_cc = "RIFF",
+        .size = 0,
+        .payload = null,
+    });
 }
