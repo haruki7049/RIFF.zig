bcba0bf9ed5713a215311968a04397fba516c109
haruki7049 <tontonkirikiri@gmail.com>
2025-08-10 12:27:48 +0900

    test(Chunk): Update

--- src/chunk.zig
+++ src/chunk.zig
@@ -20,7 +20,7 @@ pub fn size(self: Self) usize {
 
 pub fn to_binary(self: Self, allocator: Allocator) ![]const u8 {
     const id_bin: []const u8 = self.id[0..];
-    const size_bin = ToBinary.size(self.size());
+    const size_bin: []const u8 = &ToBinary.size(self.size());
     const four_cc_bin: []const u8 = self.four_cc[0..];
     const data_bin: []const u8 = self.data;
 
@@ -28,22 +28,35 @@ pub fn to_binary(self: Self, allocator: Allocator) ![]const u8 {
     defer result.deinit();
 
     try result.appendSlice(id_bin);
-    try result.appendSlice(&size_bin);
+    try result.appendSlice(size_bin);
     try result.appendSlice(four_cc_bin);
     try result.appendSlice(data_bin);
 
     return result.toOwnedSlice();
 }
 
+test "size" {
+    const info_chunk: Self = Self{
+        .id = .{ 'i', 'n', 'f', 'o' },
+        .four_cc = .{ 'I', 'N', 'F', 'O' },
+        .data = "THIS IS EXAMPLE DATA",
+    };
+
+    try testing.expectEqual(info_chunk.size(), 24);
+}
+
 test "to_binary" {
     const allocator = testing.allocator;
     const info_chunk: Self = Self{
         .id = .{ 'i', 'n', 'f', 'o' },
-        .four_cc = .{ ' ', ' ', ' ', ' ' },
+        .four_cc = .{ 'I', 'N', 'F', 'O' },
         .data = "THIS IS EXAMPLE DATA",
     };
     const info_chunk_data: []const u8 = try info_chunk.to_binary(allocator);
     defer allocator.free(info_chunk_data);
 
-    try testing.expectEqualStrings(info_chunk_data, @embedFile("./riff_files/only_chunk/info.riff"));
+    testing.expect(std.mem.eql(u8, info_chunk_data, @embedFile("./riff_files/only_chunk/info.riff"))) catch {
+        std.debug.print("info_chunk_data: {x}\n", .{info_chunk_data});
+        return error.TestUnexpectedResult;
+    };
 }
